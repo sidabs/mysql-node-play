39b8843a71d43ec7aafcd7613ab031b849c97b6a
Siddhartha Dabral <siddhartha.dabral@gmail.com>
2014-06-28 20:25:45 -0400

    added helpers function for db functionality, updated end points

--- node-mysql/routes/users.js
+++ node-mysql/routes/users.js
@@ -1,50 +1,58 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var helpers = {
+	connectionOptions: {
+		host		:	'localhost',
+		user		:	'root',
+		password	:	'sd',
+		database	:	'usersone'
+	},
+	createConnection: function(options) {
+		var connection = mysql.createConnection(options);
+		return connection;
+	},
+	startConnection: function(connection) {
+		connection.connect();
+	},
+	endConnection: function(connection) {
+		connection.end();
+	},
+	query: function(connection, query, callback) {
+		connection.query(query, function(dbError, dbRows, dbFields) {
+			callback(dbError, dbRows, dbFields);
+		});
+	}
+}
+
+
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
 router.get('/create/:name/:email', function(req, res) {
-	var mysql      = require('mysql');
-	var connection = mysql.createConnection({
-	  host		:	'localhost',
-	  user		:	'root',
-	  password	:	'sd',
-	  database	:	'usersone'
-	});
+	var id = new Date().getTime();
+	var name = req.params['name'];
+	var email = req.params['email'];
 
-	connection.connect();
-
-
-	id = new Date().getTime();
-	name = req.params['name'];
-	email = req.params['email'];
-
-	var query = 'insert into users values ("' + id + '", "' + name + '", "' + email + '")'
+	var query = 'insert into users values ("' + id + '", "' + name + '", "' + email + '")';
 	console.log('query: ', query);
 
-	connection.query(query, function(err, result) {
-	  if (err) throw err;
-
-	  res.send(result);
+	var dbConn = helpers.createConnection(helpers.connectionOptions);
+	helpers.startConnection(dbConn);
+	helpers.query(dbConn, query, function(dbError, dbRows, dbFields) {
+		res.json({
+			error:	dbError,
+			rows:	dbRows,
+			fields:	dbFields
+		});
 	});
-
-	connection.end();
+	helpers.endConnection(dbConn);
 });
 
 router.get('/get/:id?', function(req, res) {
-	var mysql      = require('mysql');
-	var connection = mysql.createConnection({
-	  host		:	'localhost',
-	  user		:	'root',
-	  password	:	'sd',
-	  database	:	'usersone'
-	});
-
-	connection.connect();
-
 	if(req.params['id']) {
 		//retrieve the user with the passed id
 		var query = 'select * from users where _id = "' + req.params['id'] + '"';
@@ -52,18 +60,20 @@ router.get('/get/:id?', function(req, res) {
 		//rertrieve all users
 		var query = 'select * from users';
 	}
-
 	console.log('query: ', query);
 
-	connection.query(query, function(err, rows, fields) {
-	  if (err) throw err;
-
-	  console.log('ROWS:', rows);
-	  console.log('FIELDS: ', fields);
-	  res.send(rows);
+	var dbConn = helpers.createConnection(helpers.connectionOptions);
+	helpers.startConnection(dbConn);
+	helpers.query(dbConn, query, function(dbError, dbRows, dbFields) {
+		res.json({
+			error:	dbError,
+			rows:	dbRows,
+			fields:	dbFields
+		});
 	});
-
-	connection.end();
+	helpers.endConnection(dbConn);
 });
+
+router.get('update/:id/:name/:email', function(req,res) {});
 
 module.exports = router;
